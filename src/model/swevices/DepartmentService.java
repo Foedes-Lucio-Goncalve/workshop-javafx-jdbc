53e91416a3048fdca9e192021fed9f8b0bdd1abe
Foedes Lucio Goncalves <foedesluciogoncalves@gmail.com>
2026-02-16 16:35:53 -0400

    acesso a dados

--- src/model/swevices/DepartmentService.java
+++ src/model/swevices/DepartmentService.java
@@ -3,16 +3,23 @@ package model.swevices;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.entities.Department;
 
+
+
 public class DepartmentService {
+	
+	private DepartmentDao dao = DaoFactory.createDepartmentDao();
 
 	public List<Department> findAll(){
-		List<Department> list = new ArrayList<>();
-		list.add(new Department(1,"Livros"));
-		list.add(new Department(2,"Computadores"));
-		list.add(new Department(3,"Eletronocos"));
-		return list;
+		//List<Department> list = new ArrayList<>();
+		//list.add(new Department(1,"Livros"));
+		//list.add(new Department(2,"Computadores"));
+		//list.add(new Department(3,"Eletronocos"));
+		// return list;
+		return dao.findAll();
 		
 	}
 }
