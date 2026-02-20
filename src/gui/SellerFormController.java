da9a03bf82a71288f20e1e562b50e6470dea98e1
Foedes Lucio Goncalves <foedesluciogoncalves@gmail.com>
2026-02-20 17:46:24 -0400

    formulario completo

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,8 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -17,6 +20,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Seller;
@@ -36,9 +40,21 @@ public class SellerFormController implements Initializable {
 
 	@FXML
 	private TextField txtName;
+	@FXML
+	private TextField txtEmail;
+	@FXML
+	private DatePicker dpBrithDate;
+	@FXML
+	private TextField txtBaseSalary;
 
 	@FXML
 	private Label labelErrorName;
+	@FXML
+	private Label labelErrorEmail;
+	@FXML
+	private Label labelErrorBrithDate;
+	@FXML
+	private Label labelErrorBaseSalary;
 
 	@FXML
 	private Button btnSave;;
@@ -65,39 +81,36 @@ public class SellerFormController implements Initializable {
 			service.saveOrUpdadte(entity);
 			notifyDataChangeListerner();
 			Utils.CurrentState(evento).close();
-		} 
-		catch (ValidationException e) {
+		} catch (ValidationException e) {
 			setErrorMessage(e.getErros());
-		}
-		 catch (DbException e) {
+		} catch (DbException e) {
 			Alerts.showAlert("erro na hora de salvar", null, e.getMessage(), AlertType.ERROR);
 		}
 	}
 
 	private void notifyDataChangeListerner() {
-		for(DataChangeListener listener : dataChangeListener) {
+		for (DataChangeListener listener : dataChangeListener) {
 			listener.onDataChanged();
 		}
-		
+
 	}
 
 	private Seller getFormData() {
 		Seller obj = new Seller();
 		ValidationException exception = new ValidationException("erro de validacao");
-		
+
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
-		
-		if(txtName.getText() == null || txtName.getText().trim().equals(""))
-		{
+
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
 			exception.addErro("name", "o campo nao pode ser vazio");
 		}
-		
+
 		obj.setName(txtName.getText());
-		
+
 		if (exception.getErros().size() > 0) {
 			throw exception;
 		}
-		
+
 		return obj;
 	}
 
@@ -121,7 +134,10 @@ public class SellerFormController implements Initializable {
 
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
-		Constraints.setTextFieldMaxLength(txtName, 30);
+		Constraints.setTextFieldMaxLength(txtName, 70);
+		Constraints.setTextFieldDouble(txtBaseSalary);
+		Constraints.setTextFieldMaxLength(txtEmail, 60);
+		Utils.formatDatePicker(dpBrithDate, "dd/MM/yyyy");
 	}
 
 	public void updateFormData() {
@@ -132,13 +148,20 @@ public class SellerFormController implements Initializable {
 
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+		txtEmail.setText(entity.getEmail());
+		Locale.setDefault(Locale.US);
+		txtBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));
+		if (entity.getBirthDate() != null) {
+			dpBrithDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
+		}
 	}
-	private void setErrorMessage(Map<String,String> erros) {
+
+	private void setErrorMessage(Map<String, String> erros) {
 		Set<String> fields = erros.keySet();
-		if(fields.contains("name")) {
+		if (fields.contains("name")) {
 			labelErrorName.setText(erros.get("name"));
 		}
-			
+
 	}
 
 }
